769ca389e331471272392156929ccc92bbf182bc
Azlan Baig <135128027+Azlan-Baig@users.noreply.github.com>
2025-02-04 15:45:39 +0500

    first commit

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,35 @@
+import { connectToDatabase } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/Models/User";
+
+export async function POST(request: NextRequest) {
+  const { email, password } = await request.json();
+  try {
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 }
+      );
+    }
+    await connectToDatabase();
+   const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+
+    await User.create({email,password});
+    return NextResponse.json(
+        { message: "User is successfully registered" },
+        { status: 201 }
+      );
+  } catch (error) {
+    return NextResponse.json(
+        { error: "Failed to register" },
+        { status: 400 }
+      );
+  }
+}
