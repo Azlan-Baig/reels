9377f4e61f172419ae0c65e35352779797cb8d59
Azlan Baig <135128027+Azlan-Baig@users.noreply.github.com>
2025-02-11 12:33:10 +0500

    Ik Provider

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,33 +1,39 @@
-"use client"
-import React from "react";
-import { ImageKitProvider, IKImage } from "imagekitio-next";
-
+"use client";
+import { ImageKitProvider } from "imagekitio-next";
+import { SessionProvider } from "next-auth/react";
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
-const authenticator = async () => {
-  try {
-    const response = await fetch("http://localhost:3000/api/auth");
 
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(`Request failed with status ${response.status}: ${errorText}`);
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const authenticator = async () => {
+    try {
+      const response = await fetch("/api/imagekit-auth");
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `Request failed with status ${response.status}: ${errorText}`
+        );
+      }
+
+      const data = await response.json();
+      const { signature, expire, token } = data;
+      return { signature, expire, token };
+    } catch (error) {
+      console.log(error);
+
+      throw new Error(`ImageKit Authentication request failed`);
     }
-
-    const data = await response.json();
-    const { signature, expire, token } = data;
-    return { signature, expire, token };
-  } catch (error:any) {
-    throw new Error(`Authentication request failed: ${error.message}`);
-  }
-};
-
-export default function Home() {
+  };
   return (
-    <div className="App">
-      <ImageKitProvider urlEndpoint={urlEndpoint} publicKey={publicKey} authenticator={authenticator}>
-        {/* ...client side upload component goes here */}
+    <SessionProvider>
+      <ImageKitProvider
+        urlEndpoint={urlEndpoint}
+        publicKey={publicKey}
+        authenticator={authenticator}
+      >
+        {children}
       </ImageKitProvider>
-      {/* ...other SDK components added previously */}
-    </div>
+    </SessionProvider>
   );
 }
